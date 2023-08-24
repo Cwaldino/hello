e3902b7a6b30113c3227a154cae819a6ec246c67
Carter Wald <Cwald2.unl.edu>
2023-08-24 13:21:01 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 /**
- *
- *
- *
- *
- * 
-*/
+ * Author: Carter Wald
+ * Date: 2023/08/22
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Carter Wald!\n");
+  printf("My major is Computer Engineering.\n");
+
+  return 0;
+}
